18d57b2ca43044f250be7e27bc28c1b8e0aebbfb
e <vbnm0149@gmail.com>
2024-02-03 22:35:32 -0500

    a

--- include/robot-config.h
+++ include/robot-config.h
@@ -16,10 +16,7 @@ extern motor CatapultLift;
 extern motor Catapult;
 extern digital_out Ratchet;
 extern digital_out CatapultRelease;
-extern digital_out CatapultBottom;
 extern digital_out MatchLoadLock;
 extern controller Controller;
-extern inertial Inertial;
-extern gps GPS;
 
 void  vexcodeInit( void );
